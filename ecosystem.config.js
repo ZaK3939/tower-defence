bddcb4b7099417125fe69529d6a25dc699c4cf1a
ZaK3939 <tsu.aria.kalei@gmail.com>
2023-09-07 01:31:27 +0400

    feat: update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
   apps: [
     {
       name: "colyseus-app",
-      script: "backend/build/index.js",
+      script: "backend/build/backend/index.js",
       time: true,
       watch: false,
       instances: os.cpus().length,
