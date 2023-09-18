8b335e435713e26eb3643278368aebca546fecf4
oji3 <oji3xx@gmail.com>
2023-09-18 11:18:17 +0800

    default-avatar

--- client/src/game/scenes/screen/interface/player-hud/avatar/index.tsx
+++ client/src/game/scenes/screen/interface/player-hud/avatar/index.tsx
@@ -8,7 +8,7 @@ export const Avatar: React.FC = () => {
   const game = useGame<IGame>();
   return (
     <Container>
-      <Image src={game.world.player.wawa?.image.x10bg || "assets/sprites/hud/avatar.png"} />
+      <Image src={game.world.player.wawa?.image.x10bg || "assets/wawa.png"} />
     </Container>
   )
 };
